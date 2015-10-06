02da088a87ea4ae5a96fa3279ec5bad2af4f4b34
joseifb <jfloresbelmares@gmail.com>
2015-10-06 10:57:08 -0600

    landing page

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var io = require('socket.io')(http);
 
 app.use(express.static(__dirname));
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/game.html');
 });
 
 /*
