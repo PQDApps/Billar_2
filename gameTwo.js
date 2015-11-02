602fe32e2586636c7e4d0361bc75a1baa30c97e2
joseifb <joseifbDDDDDDDDDDDDDDDDDDDDDD>
2015-11-01 20:53:42 -0700

    test new graphics

--- gameTwo.js
+++ gameTwo.js
@@ -4,6 +4,22 @@ var Pool = {
     YELLOW: 1,
     WHITE: 2,
     BLACK: 3,
+   /* white: 0,
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eigth: 8,
+    nine: 9,
+    ten: 10,
+    eleven: 11,
+    twelve: 12,
+    thirteen: 13,
+    fourteen: 14,
+    fifteen: 15,*/
 };
 
 var socket = io();
@@ -32,7 +48,7 @@ Pool.Preloader.prototype = {
 
         this.load.images([ 'logo', 'tableTwo.fw', 'cue', 'fill', 'line']);
 
-        this.load.spritesheet('balls', 'balls.png', 26, 26);
+        this.load.spritesheet('balls', 'balls3.png', 26, 26);
 
         this.load.physics('table');
 
